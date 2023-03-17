70b990dc80433ab187c6a14a6d5001e8a51bbd8c
gabriel <gaamdal557@gmail.com>
2023-03-17 13:51:48 -0300

    model investimento

---
+++ API-Priori/Models/Investimento.cs
@@ -0,0 +1,15 @@
+﻿namespace API_Priori.Models;
+
+public class Investimento
+{
+    public int InvestimentoId { get; set; }
+    public int RiscoInvestimentoId { get; set; }
+    public string? Nome { get; set; }
+    public string? TipoInvestimento { get; set; }
+    public int RentabilidadeFixa { get; set; }
+    public int RentabilidadeVariavel { get; set; }
+    public DateTime DataAtualizacao { get; set; }
+    public DateTime Vencimento { get; set; }
+    public int ValorMinimo { get; set; }
+    public int TempoMinimo { get; set; }
+}
