a55afb083e61f4172ec810c877b5b1405a8f3356
gabriel <gaamdal557@gmail.com>
2023-03-17 14:18:47 -0300

    model carteira investimento

---
+++ API-Priori/Models/CarteiraInvestimento.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace API_Priori.Models;
+
+public class CarteiraInvestimento
+{
+    public int CarteiraInvestimentoId { get; set; }
+    public int ClienteId { get; set; }
+    public int InvestimentoId { get; set; }
+    public double RentabilidadeFixa { get; set; }
+    public double RentabilidadeVariavel { get; set; }
+    public DateTime DataEfetuacao { get; set; }
+    public Double ValorAplicado { get; set; }
+    public DateTime DataEncerramento { get; set; }
+    public string Status { get; set; }
+    public double Saldo { get; set; }
+}
