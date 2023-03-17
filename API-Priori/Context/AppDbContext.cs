df19010f3c348f4474891f7bb9eb0dc18a3ef61c
gabriel <gaamdal557@gmail.com>
2023-03-17 14:43:48 -0300

    classe contexto

---
+++ API-Priori/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using API_Priori.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Priori.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Atualizacao> Atualizacaos { get; set; }
+    public DbSet<CarteiraInvestimento> CarteiraInvestimentos { get; set; }
+
+    public DbSet<Cliente> Clientes { get; set; }
+
+    public DbSet<Consultor> Consultors { get; set; }
+
+    public DbSet<Investimento> Investimentos { get; set; }
+
+}
