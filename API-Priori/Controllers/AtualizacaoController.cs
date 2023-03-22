933b1d1837e454ca856c5e227e175b1e961cc38e
gabriel <gaamdal557@gmail.com>
2023-03-22 11:28:16 -0300

    atualizacao e carteira controller

---
+++ API-Priori/Controllers/AtualizacaoController.cs
@@ -0,0 +1,77 @@
+﻿using API_Priori.Models;
+using API_Priori.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Priori.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AtualizacaoController : ControllerBase
+    {
+        private readonly IUnitOfWork _uof;
+
+        public AtualizacaoController(IUnitOfWork context)
+        {
+            _uof = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Atualizacao>> GetAll()
+        {
+            var atualizcao = _uof.AtualizacaoRepository.GetAll().ToList();
+            if (atualizcao is null)
+                return NotFound("Atualização não encontrada");
+
+            return atualizcao;
+        }
+
+        [HttpGet("{id}", Name = "ObterAtualizacao")]
+        public ActionResult<Atualizacao> GetAtualizacaoById(int id)
+        {
+            var atualizacao = _uof.AtualizacaoRepository.GetById(a => a.AtualizacaoId == id);
+            if (atualizacao is null)
+                return NotFound();
+
+            return atualizacao;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Atualizacao atualizacao)
+        {
+            if (atualizacao is null)
+                return BadRequest();
+            
+            _uof.AtualizacaoRepository.Add(atualizacao);
+            _uof.Commit();
+
+            return new CreatedAtRouteResult("ObterAtualizacao",
+               new { id = atualizacao.AtualizacaoId }, atualizacao);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Atualizacao atualizacao)
+        {
+          if(id != atualizacao.AtualizacaoId)
+                return BadRequest();
+
+            _uof.AtualizacaoRepository.Update(atualizacao);
+            _uof.Commit();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var atualizacao = _uof.AtualizacaoRepository.GetById(a => a.AtualizacaoId == id);
+            if(atualizacao is null)
+                return NotFound();
+
+            _uof.AtualizacaoRepository.Delete(atualizacao);
+            _uof.Commit();
+
+            return Ok();    
+        }
+    }
+}
---
+++ API-Priori/Controllers/CarteiraInvestimentoController.cs
@@ -0,0 +1,77 @@
+﻿using API_Priori.Models;
+using API_Priori.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Priori.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarteiraInvestimentoController : ControllerBase
+    {
+        private readonly IUnitOfWork _uof;
+
+        public CarteiraInvestimentoController(IUnitOfWork context)
+        {
+            _uof = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CarteiraInvestimento>> GetAllCarteiraInvetimento()
+        {
+            var carteira = _uof.CarteiraInvestimentoRepository.GetAll().ToList();
+            if(carteira is null)
+                return NotFound();
+
+            return carteira;
+        }
+
+        [HttpGet("{id}", Name = "ObterCarteira")]
+        public ActionResult<CarteiraInvestimento> GetCarteiraById(int id)
+        {
+            var carteira = _uof.CarteiraInvestimentoRepository.GetById(c => c.CarteiraInvestimentoId == id);
+            if (carteira is null)
+                return NotFound();
+
+            return carteira;
+        }
+
+        [HttpPost]
+        public ActionResult Post(CarteiraInvestimento carteira)
+        {
+            if (carteira is null)
+                return BadRequest();
+
+            _uof.CarteiraInvestimentoRepository.Add(carteira);
+            _uof.Commit();
+
+            return new CreatedAtRouteResult("ObterCarteira",
+                new { id = carteira.CarteiraInvestimentoId }, carteira);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, CarteiraInvestimento carteira)
+        {
+            if (id != carteira.CarteiraInvestimentoId)
+                return BadRequest();
+
+            _uof.CarteiraInvestimentoRepository.Update(carteira);
+            _uof.Commit();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var carteira = _uof.CarteiraInvestimentoRepository.GetById(c => c.CarteiraInvestimentoId == id);
+            if (carteira is null)
+                return NotFound();
+
+            _uof.CarteiraInvestimentoRepository.Delete(carteira);
+            _uof.Commit();
+
+            return Ok();
+        }
+    }
+}
